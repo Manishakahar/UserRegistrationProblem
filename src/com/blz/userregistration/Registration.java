3c603e9e25931ca73973e7531f91dc29053636d1
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-15 16:02:24 +0530

    Added: checked last name is valid or invaild

--- src/com/blz/userregistration/Registration.java
+++ src/com/blz/userregistration/Registration.java
@@ -5,22 +5,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Registration {
-    public static void validateFirstName(String firstName) {
+    public static void validateLastName(String lastName) {
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(firstName);
+        Matcher matcher = pattern.matcher(lastName);
         if (matcher.matches()) {
-            System.out.println("First name is valid");
+            System.out.println("Last name is valid");
         } else {
-            System.out.println("First name is invalid");
+            System.out.println("Last name is invalid");
         }
     }
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
-        System.out.println("Enter First Name :");
+        System.out.println("Enter the Last Name :");
         Scanner sc = new Scanner(System.in);
-        String firstName = sc.next();
-        validateFirstName(firstName);
+        String lastName = sc.next();
+        validateLastName(lastName);
 
     }
 }
